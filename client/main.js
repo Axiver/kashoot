cbf957d1f17d1a892496adee0d5d6faa0f822288
Garlicvideos <Garlicvideos@gmail.com>
2019-11-02 13:49:06 +0800

    color changing background
    bg color of index page changes now

---
+++ client/main.js
@@ -0,0 +1,17 @@
+//Functions
+function changeColor(color) {
+	$("body").css("background-color", "rgb(" + color + ")");
+	console.log("called");
+}
+
+function cycleColor() {
+	changeColor("9, 227, 24");
+	setTimeout("changeColor('227, 118, 9')", 8000);
+	setTimeout("changeColor('202, 227, 9')", 16000);
+	setTimeout("changeColor('9, 227, 212')", 24000);
+}
+
+$(document).ready(function() {
+	cycleColor();
+    setInterval(cycleColor, 32000);
+});
